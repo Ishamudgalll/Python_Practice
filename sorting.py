99180ee6fbb47945ee38565aa073d8d0db4e7bec
Ishamudgalll <47916019+Ishamudgalll@users.noreply.github.com>
2020-05-28 20:20:39 +0530

    Update and rename mans.py to sorting.py

--- sorting.py
+++ sorting.py
@@ -1,11 +1,10 @@
 badList = [12, 5, 13, 8, 9, 65]
 
+length = len(badList) - 1
+unsorted = True
 
-    length = len(badList) - 1
-    unsorted = True
-
-    while unsorted:
-        for element in range(length):
+while unsorted:
+    for element in range(length):
             unsorted = False
             if badList[element] > badList[element + 1]:
                 hold = badList[element + 1]
